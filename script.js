20dc9a9945f447164982368d0146919e5bb4bbe4
xHandre <146939769+xHandre@users.noreply.github.com>
2025-02-01 09:19:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function moveNoButton() {
     setTimeout(() => {
         wrongAnswer.classList.remove("flash");
         wrongAnswer.classList.add("hidden");
-    }, 700);
+    }, 1000);
 }
 
 // Function to create multiple "Yes" buttons based on noClickCount
